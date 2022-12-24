5a26d37be114bc6870bd580b1d860895ae805cb1
trtv404 <tr_tatjana@mail.ru>
2022-12-24 12:30:02 +0700

    task 13 is done

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+using static System.Console;
+
+WriteLine("Введите любое число");
+int num = Convert.ToInt32(ReadLine());
+if (num < 100) {
+WriteLine("Третьей цифры нет");
+} else {
+num = num / 100;
+num = num % 10;
+WriteLine($"Третья цифра - {num}");
+}
