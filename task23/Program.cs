bc66e675ec63f93be69392f8b5b178baf07900b6
trtv404 <tr_tatjana@mail.ru>
2022-12-25 23:08:44 +0700

    task 23 is done

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+using static System.Console;
+
+WriteLine("Введите число");
+bool isParse = int.TryParse(ReadLine(), out int n);
+if (!isParse) {
+    WriteLine("Введено неверное значение");
+    return;
+}
+
+int i = 1;
+while (i <= n) 
+{
+    WriteLine($"Кубом числа {i} является {Math.Pow(i,3)}");
+    i++;
+}
