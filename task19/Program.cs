bebf447210c9dfc287204836c798d5ebec1ec246
trtv404 <tr_tatjana@mail.ru>
2022-12-25 22:59:20 +0700

    task 19 is done

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,3 +1,18 @@
 ﻿// Задача 19
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, 
 // является ли оно палиндромом.
+using static System.Console;
+
+WriteLine("Введите пятизначной число");
+int num = Convert.ToInt32(ReadLine());
+if (num < 10000 || num > 99999)
+ {
+WriteLine("Введено неверное число");
+return;
+}
+int num1 = num / 10000;
+int num2 = num / 1000 % 10;
+int num4 = num / 10 % 10;
+int num5 = num % 10;
+
+WriteLine(num1 == num5 && num2 == num4 ? "Число является полиндромом" : "Число не является полиндромом");
