aac61298d62d94e5cf905433873b2d1ed42b2f5d
trtv404 <tr_tatjana@mail.ru>
2023-01-26 01:31:15 +0700

    task 27 is done

--- task27/Program.cs
+++ task27/Program.cs
@@ -3,3 +3,26 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+using static System.Console;
+
+Write("input a number ");
+int number = int.Parse(ReadLine()!);
+
+int sum = GetSum(number);
+
+WriteLine($"Sum of numbers = {sum}");
+
+int GetSum(int a) {
+
+int div = 10;
+int mod = 0;
+int result = 0; 
+if (a < 0) a = a * -1;
+while (a > 0) {
+    mod = a % div;
+    result = result + mod;
+    a = a / div;
+    }
+return result;
+}
