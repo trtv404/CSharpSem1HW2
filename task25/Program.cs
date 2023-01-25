daa307e2afe7721dc77dce3da72e3787b6083444
trtv404 <tr_tatjana@mail.ru>
2023-01-26 00:49:03 +0700

    task 25 is done

--- task25/Program.cs
+++ task25/Program.cs
@@ -4,3 +4,18 @@
 // В задаче не использвать стандартный метод Pow!
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+using static System.Console;
+
+Write("input first number ");
+int a = int.Parse(ReadLine()!);
+Write("input second number ");
+int b = int.Parse(ReadLine()!);
+
+int result = 1;
+
+for (int i = 0; i < b; i++) 
+    {
+    result = result * a;  
+    }
+
+WriteLine($"{a} to the power of {b} = {result}");
