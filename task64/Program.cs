9895bad26d8be8788ff9719490e35e2432330374
trtv404 <tr_tatjana@mail.ru>
2023-02-05 20:08:00 +0700

    task 64 is done

---
+++ task64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 64: Напишите программу, которая реализует рекурсивный 
+// метод вывода всех натуральных числел в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+using static System.Console;
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+PrintNumbers(number);
+
+void PrintNumbers(int num)
+{
+    if (num < 1) Write("неверное число");
+    if (num == 1) Write(num);
+    else
+    {
+        Write($"{num}, ");
+        PrintNumbers(num - 1);
+    }
+}
+
+
