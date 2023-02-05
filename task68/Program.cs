1fd5bf967eef22b9c90fc4699351b086e56fc02d
trtv404 <tr_tatjana@mail.ru>
2023-02-05 20:43:13 +0700

    task 68 is done

---
+++ task68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 68: Напишите программу реализующую метод 
+// вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+using static System.Console;
+
+Write("Введите два неотрицательных числа через пробел: ");
+string[] inPut = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int number1 = int.Parse(inPut[0]); 
+int number2 = int.Parse(inPut[1]);
+WriteLine(Ack(number1, number2));
+
+int Ack(int num1, int num2)
+{
+    if (num1 == 0) return (num2 + 1);
+    else if (num2 == 0) return Ack (num1-1, 1);
+    else return Ack(num1-1, Ack(num1, num2-1));
+}
